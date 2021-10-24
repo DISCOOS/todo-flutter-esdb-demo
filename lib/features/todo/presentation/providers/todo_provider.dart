276077f200579354dd9f11a9cdcef7709b3789d4
Kenneth Gulbrandsoy <kenneth@discoos.org>
2021-10-24 12:55:23 +0200

    Only notify listeners on new modifications

--- lib/features/todo/presentation/providers/todo_provider.dart
+++ lib/features/todo/presentation/providers/todo_provider.dart
@@ -6,7 +6,12 @@ import 'package:todo_flutter_esdb_demo/features/todo/domain/repositories/todo_st
 
 class TodoProvider extends ChangeNotifier {
   TodoProvider(this._store) {
-    _store.onReceived().forEach((e) => notifyListeners());
+    _store.onReceived().forEach((e) {
+      if (_store.modifications > _seen) {
+        _seen = _store.modifications;
+        notifyListeners();
+      }
+    });
   }
 
   final TodoStore _store;
@@ -20,6 +25,7 @@ class TodoProvider extends ChangeNotifier {
 
   Duration get duration => _store.duration;
   int get modifications => _store.modifications;
+  int _seen = 0;
 
   Future<void> load() => _store.load();
 
